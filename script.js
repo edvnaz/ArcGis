aea581c9a41075b2802803db6b69f7c6d908e6c2
Ed <edvnaz@gmail.com>
2017-10-25 20:51:48 +0300

    first commit

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ require(["esri/Color",
     var map, gp, toolbar, graphic;
     var featureSet = new FeatureSet();
     var features= [];
-    
+
     parser.parse();
 
     app = {
@@ -50,68 +50,67 @@ require(["esri/Color",
     populationMap.setOpacity(0);
     map.addLayer(populationMap);
 
-        //identify proxy page to use if the toJson payload to the geoprocessing service is greater than 2000 characters.
-        //If this null or not available the gp.execute operation will not work.  Otherwise it will do a http post to the proxy.
-        esriConfig.defaults.io.proxyUrl = "/proxy/";
-        esriConfig.defaults.io.alwaysUseProxy = false;
+    //identify proxy page to use if the toJson payload to the geoprocessing service is greater than 2000 characters.
+    //If this null or not available the gp.execute operation will not work.  Otherwise it will do a http post to the proxy.
+    esriConfig.defaults.io.proxyUrl = "/proxy/";
+    esriConfig.defaults.io.alwaysUseProxy = false;
 
-        function initTools(evtObj) {
-          gp = new Geoprocessor("https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/GPServer/PopulationSummary");
-          gp.setOutSpatialReference({wkid:102100}); 
-          gp.on("execute-complete", displayResults);
+    function initTools(evtObj) {
+      gp = new Geoprocessor("https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/GPServer/PopulationSummary");
+      gp.setOutSpatialReference({wkid:102100}); 
+      gp.on("execute-complete", displayResults);
 
-          app.toolbar = toolbar = new Draw(evtObj.map);
-          toolbar.on("draw-complete", computeZonalStats);
-        }
-
-        function computeZonalStats(evtObj) {
-          var geometry = evtObj.geometry;
-          map.showZoomSlider();
-
-          var symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID).setWidth(1.5),
-            new Color([0, 0, 0, 0.25])
-            );
-          graphic = new Graphic(geometry,symbol);
-
-          map.graphics.add(graphic);
-          toolbar.deactivate();
-
-          features.push(graphic);
-
-          featureSet.features = features;
-        }
-
-        function displayResults(evtObj) {
-          var results = evtObj.results;
-          var content = string.substitute("The population in the user defined polygon is ${number:dojo.number.format}.",{number:results[0].value.features[0].attributes.SUM});
-
-          alert(content);
-        }
-
-        function compute(){
-          var params = { "inputPoly":featureSet };
-          gp.execute(params);
-        }
-
-
-        function clean(){
-          features= [];
-          featureSet = new FeatureSet();
-          map.graphics.clear();
-        }
-
-      });
-
-    // Soninio meniu atidarymas, uzdarymas
-    function openNav() {
-      if(document.getElementById("mySidenav").style.width == "0px"){
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        $('#arrow').removeClass('glyphicon glyphicon-chevron-right').addClass('glyphicon glyphicon-chevron-left');
-      }else{
-        document.getElementById("mySidenav").style.width = "0px";
-        document.getElementById("main").style.marginLeft = "0px";
-        $('#arrow').removeClass('glyphicon glyphicon-chevron-left').addClass('glyphicon glyphicon-chevron-right');
-      }
+      app.toolbar = toolbar = new Draw(evtObj.map);
+      toolbar.on("draw-complete", computeZonalStats);
     }
+
+    function computeZonalStats(evtObj) {
+      var geometry = evtObj.geometry;
+      map.showZoomSlider();
+
+      var symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+        new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID).setWidth(1.5),
+        new Color([0, 0, 0, 0.25])
+        );
+      graphic = new Graphic(geometry,symbol);
+
+      map.graphics.add(graphic);
+      toolbar.deactivate();
+
+      features.push(graphic);
+
+      featureSet.features = features;
+    }
+
+    function displayResults(evtObj) {
+      var results = evtObj.results;
+      var content = string.substitute("The population in the user defined polygon is ${number:dojo.number.format}.",{number:results[0].value.features[0].attributes.SUM});
+
+      alert(content);
+    }
+
+    function compute(){
+      var params = { "inputPoly":featureSet };
+      gp.execute(params);
+    }
+
+
+    function clean(){
+      features= [];
+      featureSet = new FeatureSet();
+      map.graphics.clear();
+    }
+  });
+
+// Soninio meniu atidarymas, uzdarymas
+function openNav() {
+  if(document.getElementById("mySidenav").style.width == "0px"){
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    $('#arrow').removeClass('glyphicon glyphicon-chevron-right').addClass('glyphicon glyphicon-chevron-left');
+  }else{
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("main").style.marginLeft = "0px";
+    $('#arrow').removeClass('glyphicon glyphicon-chevron-left').addClass('glyphicon glyphicon-chevron-right');
+  }
+}
